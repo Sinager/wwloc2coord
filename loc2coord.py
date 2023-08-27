ce9ca2ddf915d4fda5041a5089a10ac627e7b930
Sinager <sinageruk@gmail.com>
2023-08-27 18:38:53 +0200

    Add files via upload

---
+++ loc2coord.py
@@ -0,0 +1,26 @@
+''
+MAIDENHEAD LOCATOR TO DECIMAL LAT/LON CONVERTER
+based on the excellent explaination by M0NWK
+2023 Stefano Sinagra IZ0MJE
+released under GNU/GPL conditions
+''
+
+locator = "jn61fu"
+
+# LATITUDE 
+lata =(ord(locator[1].upper())-65)*10
+latb =int(locator[3])
+latc =(ord(locator[5].lower())-97)/24+(1/48)-90
+latitude = lata + latb + latc
+
+#LONGITUDE
+lona =(ord(locator[0].upper())-65)*20
+lonb =int(locator[2])*2
+lonc =(ord(locator[4].lower())-97)/12+(1/24)
+longitude = (lona + lonb + lonc) -180
+
+
+print("Latitude: ", end="")
+print(latitude)
+print("Longitude: ", end="")
+print(longitude) 
